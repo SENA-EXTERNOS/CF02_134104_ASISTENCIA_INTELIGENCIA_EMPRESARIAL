ccc49b4c17ebba0e0a2f08798a1aa5c0dd6c5e01
Andrea Botello <andrea.botellor@misena.edu.co>
2023-04-05 13:40:44 -0500

    ajustes

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -536,7 +536,7 @@
 
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/t5ZGAvfxS8I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         figcaption Video: Leyenda del video 
 
     .row.align-items-end.bg3.h4.p-3.mt-5 Ciclo de servicio
